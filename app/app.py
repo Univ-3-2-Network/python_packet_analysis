8f6d259a4566a34d557ddf0036bc14faebf62142
prayanne <kyhy0321@gmail.com>
2025-11-25 01:12:34 +0900

    edit app.py / by docs

--- app/app.py
+++ app/app.py
@@ -1,5 +1,13 @@
+import sys
 from scapy.all import *
 
-print(packet.show())
+if __name__ == '__main__':
+    p=sr1(IP(dst=sys.argv[1])/ICMP())
+    if p:
+        # p.show()
+        packet_callback(p)
 
-sniff(prn=packet_callback, count=10) #10개의 패킷 캡처
+
+def packet_callback(packet):
+    print(packet.show())
+    sniff(prn=packet_callback, count=10) #10개의 패킷 캡처
